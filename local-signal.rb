a11cf19768c8b2e435df3e3a8e8fdb1c207b48b1
S. Christoffer Eliesen <christoffer@eliesen.no>
2016-07-22 08:36:29 +0200

    Initial commit: Basic setup.

---
+++ local-signal.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler'
+Bundler.require :default
+
+origin = 'http://localhost:4000'
+
+$buffer = { server: [], client: [] }
+
+# These are queue names. You can put something into a queue, or fetch an entry
+# from it.
+put '/server' do
+  request.body.rewind
+  $buffer[:server] << request.body.read
+
+  response['Access-Control-Allow-Origin'] = origin
+  status 200
+end
+
+options '/server' do
+  response['Access-Control-Allow-Methods'] = 'PUT'
+  response['Access-Control-Allow-Origin'] = origin
+end
+
+get '/server' do
+  response['Access-Control-Allow-Origin'] = origin
+  $buffer[:server].shift
+end
+
+# Client queue:
+put '/client' do
+  request.body.rewind
+  $buffer[:client] << request.body.read
+
+  response['Access-Control-Allow-Origin'] = origin
+  status 200
+end
+
+get '/client' do
+  response['Access-Control-Allow-Origin'] = origin
+  $buffer[:client].shift
+end
+
+get '/' do
+  (<<-EOS) % [$buffer[:server].count, $buffer[:client].count]
+    Entries in queues:
+    server: %s
+    client: %s
+  EOS
+end
